41d461b3eb743083d5bd71dc7fc233d51b4fb10e
Angel1760 <59674546+Angel1760@users.noreply.github.com>
2022-05-09 13:24:32 -0700

    Add files via upload

--- source.cpp
+++ source.cpp
@@ -88,7 +88,7 @@ int main() {
  Sdisk sdisk= Sdisk("sdisk.txt",256,128);
  Filesys fsys= Filesys("sdisk.txt",256,128);
  Shell shell= Shell("sdisk.txt",256,128);
- Table table= Table("sdisk.txt",256,128);
+ Table table= Table("sdisk.txt",256,128, "flatfile", "indexfile");
  table.build_table("data.txt");
  
  string s;
@@ -103,47 +103,51 @@ int main() {
        getline(cin,s);
       
        int firstblank=s.find(' ');
-       if (firstblank < s.length()) s[firstblank]='#';
-       
+
+       if (firstblank < s.length())
+       {
+          s[firstblank]='#';
+       }
        int secondblank=s.find(' ');
        command=s.substr(0,firstblank);
       
        if (firstblank < s.length())
+       {
          op1=s.substr(firstblank+1,secondblank-firstblank-1);
-       
+       }
        if (secondblank < s.length())
+       {
          op2=s.substr(secondblank+1);
-       
+       }
        if (command=="dir")
-          {
-            // use the ls function
-           }
+       {
+           shell.dir();           // use the ls function
+       }
        
        if (command=="add")
-          {
-            // The variable op1 is the new file and op2 is the file data
-           }
+       {
+        shell.add(op1, op2);   // The variable op1 is the new file and op2 is the file data
+       }
        
        if (command=="del")
-          {
-            // The variable op1 is the file
-           }
+       {
+        shell.del(op1);        // The variable op1 is the file
+       }
        
        if (command=="type")
-          {
-            // The variable op1 is the file
-           }
+       {
+        shell.type(op1);       // The variable op1 is the file
+       }
        
        if (command=="copy")
-          {
-            // The variable op1 is the source file and the variable op2 is the destination file.
-           }
+       {
+         shell.copy(op1, op2);   // The variable op1 is the source file and the variable op2 is the destination file.
+       }
        
        if (command=="search")
-          {
-            // This is the command for Project 4
-            // The variable op1 is the date
-           }
+       {
+        table.search(op1);
+       }
        
       }
  return 0;
